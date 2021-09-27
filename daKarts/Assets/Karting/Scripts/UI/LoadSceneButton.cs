ddff97504e5828a1cd69ddd5eaa111c19159c657
Dustin McDaniel <dmakk767.coder@gmail.com>
2021-09-27 15:21:07 -0400

    updates to splash screen
    added spacebar brake into controls
    changed the background of splash screen
    jazzed up the karts visuals a little
    -failed to add my face to the head

--- daKarts/Assets/Karting/Scripts/UI/LoadSceneButton.cs
+++ daKarts/Assets/Karting/Scripts/UI/LoadSceneButton.cs
@@ -6,7 +6,7 @@ namespace KartGame.UI
     public class LoadSceneButton : MonoBehaviour
     {
         [Tooltip("What is the name of the scene we want to load when clicking the button?")]
-        public string SceneName = "daKartsSunCity";
+        public string SceneName;
 
         public void LoadTargetScene() 
         {
